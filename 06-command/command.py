bcaae01d662ad6e708a52097ddb3808df653513f
Bastian Venthur <venthur@debian.org>
2012-10-02 16:24:20 +0200

    Added command.

---
+++ 06-command/command.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+
+
+class Command(object):
+
+    def execute(self):
+        pass
+
+    def undo(self):
+        pass
+
+
+class NullCommand(Command):
+
+    def execute(self):
+        print 'Null.'
+
+    def undo(self):
+        print 'Undo Null.'
+
+
+class Remote(object):
+
+    def __init__(self):
+        self.onCommands = [NullCommand() for i in range(7)]
+        self.offCommands = [NullCommand() for i in range(7)]
+        self.undoCommand = NullCommand()
+
+    def setCommand(self, slot, onCommand, offCommand):
+        self.onCommands[slot] = onCommand
+        self.offCommands[slot] = offCommand
+
+    def onPushed(self, slot):
+        self.onCommands[slot].execute()
+        self.undoCommand = self.onCommands[slot]
+
+    def offPushed(self, slot):
+        self.offCommands[slot].execute()
+        self.undoCommand = self.offCommands[slot]
+
+    def undoPushed(self):
+        self.undoCommand.undo()
+
+
+class LightOffCommand(Command):
+
+    def execute(self):
+        print 'Light off.'
+
+    def undo(self):
+        print 'Light on.'
+
+
+class LightOnCommand(Command):
+
+    def execute(self):
+        print 'Light on.'
+
+    def undo(self):
+        print 'Light off.'
+
+
+class CeillingFan(object):
+
+    def __init__(self):
+        self.speed = 0
+
+    def high(self):
+        print 'Fan - high'
+        self.speed = 10
+
+    def medium(self):
+        print 'Fan - medium'
+        self.speed = 7
+
+    def low(self):
+        print 'Fan - low'
+        self.speed = 4
+
+    def off(self):
+        print 'Fan - off'
+        self.speed = 0
+
+
+class CeillingFanHighCommand(Command):
+
+    def __init__(self, ceillingFan):
+        self.ceillingFan = ceillingFan
+
+    def execute(self):
+        self.prevSpeed = self.ceillingFan.speed
+        self.ceillingFan.high()
+
+    def undo(self):
+        if self.prevSpeed == 0:
+            self.ceillingFan.off()
+        elif self.prevSpeed == 4:
+            self.ceillingFan.low()
+        elif self.prevSpeed == 7:
+            self.ceillingFan.medium()
+        elif self.prevSpeed == 10:
+            self.ceillingFan.high()
+
+
+class CeillingFanLowCommand(Command):
+
+    def __init__(self, ceillingFan):
+        self.ceillingFan = ceillingFan
+
+    def execute(self):
+        self.prevSpeed = self.ceillingFan.speed
+        self.ceillingFan.low()
+
+    def undo(self):
+        if self.prevSpeed == 0:
+            self.ceillingFan.off()
+        elif self.prevSpeed == 4:
+            self.ceillingFan.low()
+        elif self.prevSpeed == 7:
+            self.ceillingFan.medium()
+        elif self.prevSpeed == 10:
+            self.ceillingFan.high()
+
+
+
+
+
+if __name__ == '__main__':
+    remote = Remote()
+
+    ceillingFan = CeillingFan()
+
+    remote.setCommand(0, LightOnCommand(), LightOffCommand())
+    remote.setCommand(1, CeillingFanHighCommand(ceillingFan), CeillingFanLowCommand(ceillingFan))
+
+    for i in range(7):
+        remote.onPushed(i)
+        remote.offPushed(i)
+        remote.undoPushed()
